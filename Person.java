4a210c31bbdcac8dd60824a04af3a4e7c232cbd2
KnightRez <rezcors@gmail.com>
2022-10-09 17:51:09 +0800

    Add Java classes

---
+++ Person.java
@@ -0,0 +1,18 @@
+public class Person
+{
+	private int ID;
+	private String firstName, lastName, gender, address;
+	private long phoneNumber;
+
+	public void DisplayPerson(){
+		System.out.println("ID: " + ID);
+		System.out.println("First Name: " + firstName);
+		System.out.println("Last Name: " + lastName);
+		System.out.println("Gender: " + gender);
+		System.out.println("Address: " + address);
+		System.out.println("Phone Number: " + phoneNumber);
+	}
+	public void GenerateID(){
+
+	}
+}
---
+++ Teacher.java
@@ -0,0 +1,124 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Teacher extends Person {
+    private int department;
+    private int designation;
+    private double salary = 0;
+    private int teachingHours;
+    private List<Teacher> teachers = new ArrayList<Teacher>();
+
+    public Teacher(int ID, String firstname, String lastname, String gender, String address, int department, int designation, int salary, int teachingHours) {
+        ID = ID;
+        this.department = department;
+        this.designation = designation;
+        this.salary = salary;
+        this.teachingHours = teachingHours;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public int getDesignation() {
+        return designation;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public int getTeachingHours() {
+        return teachingHours;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setDesignation(int designation) {
+        this.designation = designation;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void setTeachingHours(int teachingHours) {
+        this.teachingHours = teachingHours;
+    }
+
+    public void DisplayEntry() {
+        DisplayPerson();
+        System.out.println("Department: " + department);
+        System.out.println("Designation: " + designation);
+        System.out.println("Salary: " + salary);
+        System.out.println("Teaching Hours: " + teachingHours);
+    }
+
+    public void addTeacher(InputTeachers())
+
+    {
+        teachers.add(entry);
+    }
+
+    public void updateTeacher(int Id, InputTeacher()
+
+    {
+        teachers.replaceAll(e -> e.getID().equals(Id) ? entry : e);
+    }
+
+    public void deleteTeacher(Int ID) {
+        teachers.removeIf(entry -> entry.getID().equals(ID));
+    }
+
+    public void calculateSalary(SearchID())
+
+    {
+        //May id na ako, yung na id na to ituturo yung designation at department
+
+
+    }
+
+    public void viewAllTeachers() {
+        for (Teacher entry : teachers) {
+            entry.DisplayEntry();
+        }
+    }
+
+    public Teacher InputTeachers() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the ID of the teacher: ");
+        int ID = scanner.nextInt();
+        System.out.println("Enter the First Name of the teacher: ");
+        String firstName = scanner.next();
+        System.out.println("Enter the Last Name of the teacher: ");
+        String lastName = scanner.next();
+        System.out.println("Enter the Gender of the teacher: ");
+        String gender = scanner.next();
+        System.out.println("Enter the Phone Number of the teacher: ");
+        Long phonenumber = scanner.nextLong();
+        System.out.println("Enter the Address of the teacher: ");
+        String address = scanner.next();
+        System.out.println("1 for Computing 2 for Business \nEnter the department of the teacher: ");
+        int department = scanner.nextInt();
+        System.out.println("1 for Head of Faculty 2 for Coordinator 3 for Lecturer \nEnter the designation of the teacher: ");
+        int designation = scanner.nextInt();
+
+
+        return teachers = new Teacher(ID, firstName, lastName, gender, address, phonenumber, department, designation);
+    }
+
+    public int SearchID(int ID) {
+        for (Teacher entry : teachers) {
+            if (entry.getID().equals(ID)) {
+                return ID;
+            }
+        }
+    }
+
+//test
+
+
+}
